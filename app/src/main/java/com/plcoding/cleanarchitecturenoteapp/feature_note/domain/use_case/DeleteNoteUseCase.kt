6c2bd5ef1e66a4b6c1d46e8279a30fabf1e274e1
noraloch <contact.nouraloudani@gmail.com>
2023-01-26 00:03:57 -0800

    vm implementation

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(
+    private val repo: NoteRepository
+) {
+    suspend operator fun invoke(note: Note) {
+        repo.deleteNote(note)
+    }
+}
--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesUseCase.kt
@@ -1,20 +1,20 @@
 package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
 
-import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
-import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+ import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
 import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetNotesUseCase(
-    private val repository: NoteRepositoryImpl
+    private val repo: NoteRepository
 ) {
     operator fun invoke(
         noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)
     ): Flow<List<Note>> {
 
-        return repository.getNotes().map { notes ->
+        return repo.getNotes().map { notes ->
             when(noteOrder.orderType) {
                 is OrderType.Ascending -> {
                     when(noteOrder) {
